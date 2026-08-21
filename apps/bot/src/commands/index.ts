c8a929c09235141b780cd360d290ce979b0d3bdf
Ghost Developer <139102805+Gh0stDeveloper@users.noreply.github.com>
2026-08-20 23:14:03 -0600

    feat: register advanced economy commands

--- apps/bot/src/commands/index.ts
+++ apps/bot/src/commands/index.ts
@@ -8,6 +8,7 @@ import { resourceCommands } from './resources.js'
 import { groupCommands } from './groups.js'
 import { securityCommands } from './security.js'
 import { economyCommands } from './economy.js'
+import { advancedEconomyCommands } from './economy-advanced.js'
 import { waifuCommands } from './waifu.js'
 import { subbotCommands } from './subbots.js'
 import { adultCommands } from './adult.js'
@@ -23,6 +24,7 @@ export const commands: BotCommand[] = [
   ...groupCommands,
   ...securityCommands,
   ...economyCommands,
+  ...advancedEconomyCommands,
   ...waifuCommands,
   ...subbotCommands,
   ...adultCommands,
