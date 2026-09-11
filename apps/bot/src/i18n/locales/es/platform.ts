8008d7d8180e046a9f4a7f5b5d78feef25573b03
Ghost Developer <139102805+Gh0stDeveloper@users.noreply.github.com>
2026-09-11 01:05:02 -0600

    feat(i18n): add Spanish platform runtime messages

---
+++ apps/bot/src/i18n/locales/es/platform.ts
@@ -0,0 +1,8 @@
+import type { TranslationCatalog } from '../../types.js'
+
+export const messages: TranslationCatalog = {
+  'discord.component.expired': 'Esta acción expiró. Ejecuta de nuevo el comando.',
+  'discord.runtime.title': 'DISCORD RUNTIME',
+  'discord.status.line': '{key}: {value}',
+  'common.information': 'Información',
+}
